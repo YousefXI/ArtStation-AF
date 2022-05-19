accfd8687c6521c0b3dcd2d0a23fb1935cf7ba0a
YousefXI <101951776+YousefXI@users.noreply.github.com>
2022-05-19 22:07:13 +0200

    not fixed navbar

--- app.js
+++
@@ -1,8 +0,0 @@
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction(){
-  if (document.body.scrollTop > 1080 || document.documentElement.scrollTop > 1080) {
-    document.getElementsByClassName("about_container").style.opacity = 1
-  }
-}
